9cdbd670d37b522caef6db811abe1e86be0243b6
h1542462994 <t1542462994@outlook.com>
2019-10-09 20:03:36 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,25 +18,11 @@ Route::get('/oauth', 'WXLoginController@oauth');
 Route::any('/wx/login', 'WXLoginController@wxLogin');
 
 Route::get('/index/info', 'IndexController@indexInfo');
-Route::any('/user/info', 'UserController@getMyInfo');
 Route::any('/config/signup', 'IndexController@registerConfig');
 Route::any('/config/campus', 'IndexController@campusConfig');
-
-Route::group(['middleware' => ['cor','check.finish']], function() {
-    Route::post('/user/register', 'UserController@register');
-    Route::post('/user/update', 'UserController@updateInfo');
-
-    Route::post('/group/list', 'GroupController@groupLists');
-    Route::post('/group/create', 'GroupController@createGroup');
-    Route::post('/group/break', 'GroupController@breakGroup');
-    Route::post('/group/submit', 'GroupController@submitGroup');
-    Route::post('/group/search', 'GroupController@searchTeam');
-    Route::post('/group/member/list', 'GroupController@getGroupMembers');
-    Route::post('/group/member/delete', 'GroupController@deleteMember');
-    Route::post('/group/info', 'GroupController@getGroupInfo');
-    Route::post('/group/update', 'GroupController@updateGroupInfo');
-    Route::post('/group/unsubmit', 'GroupController@unSubmitGroup');
-    Route::post('/group/leave', 'GroupController@leaveGroup');
+Route::any('/config/walkpath', 'IndexController@walkpathConfig');
+Route::any('/config/school', 'IndexController@schoolConfig');
+Route::any('/config/members_count', 'IndexController@membersCount');
 
 Route::group(['middleware' => ['check.wechat']], function () {
     Route::post('/user/info', 'UserController@getMyInfo');
@@ -66,4 +52,3 @@ Route::group(['middleware' => ['check.wechat']], function () {
         Route::post('/apply/count', 'ApplyController@getApplyCount');
     });
 });
-
